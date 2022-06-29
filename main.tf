13bb5604483b06e396fc4b50df794c92095e479e
sourav9199 <108008862+sourav9199@users.noreply.github.com>
2022-06-29 21:02:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,9 +4,9 @@ provider "aws" {
 }
 
 module "test" {
-  source  = "app.terraform.io/abcd92/test/ec2"
+  source  = "app.terraform.io/abcd92/test1/ec2"
   version = "1.1.1"
   ec2_ami=var.ami
   instance_type= var.instance
-  # insert required variables here
 }
+  
